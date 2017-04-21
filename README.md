6f65dd7606035c750219581edacdc9c795885f24
Anthony Kirby <akirby@teradici.com>
2017-04-21 15:31:52 -0700

    Initial release of Cloud Access Software deployment scripts for Azure.

---
+++ README.md
@@ -0,0 +1,92 @@
+# Deploy Teradici Cloud Access Software on Azure
+
+Use these steps to deploy a Virtual Machine on Azure, running Windows Server 2016, and the latest (v2.8) [Teradici Cloud Access Software](http://www.teradici.com/products-and-solutions/pcoip-products/cloud-access-software) - Standard Edition or Graphics Edition. 
+
+All resources required to run the Teradici PCoIP Standard or Graphics agent (components of Teradici Cloud Access Software) will be created and configured in your Azure subscription under a new resource group.
+
+Once deployed you will be able to connect to the Teradici PCoIP agent using the Teradici PCoIP client.
+
+## Before You Begin
+
+* Setup a Microsoft Azure account and ensure you have permissions to create new resource groups and resources. If you don't already have an account, you can sign up for a [free Microsoft Azure account](https://azure.microsoft.com/free/). 
+* Use an existing or [trial](http://connect.teradici.com/cas-trial) Registration code for a Teradici PCoIP Standard or Graphics agent.
+* Download and install the [Teradici PCoIP Client](http://www.teradici.com/product-finder/client-downloads).
+
+## Deploy the PCoIP Agent
+
+Choose whether you want to deploy a Teradici PCoIP Standard or Graphics agent and deploy it to Azure by clicking the *Deploy to Azure* button.
+
+By clicking one of the *Deploy to Azure* buttons below you accept the terms of the Teradici Cloud Access Software [End User License Agreement](http://www.teradici.com/pdf/teradici-cloud-access-software-eula.pdf) and by clicking the *Deploy to Azure* button to deploy a Teradici PCoIP Graphics agent on a NV instance type virtual machine, you have read and agree to be bound by the [software license](http://www.nvidia.com/content/DriverDownload-March2009/licence.php?lang=us) for use of the third-party drivers.
+   
+#### Teradici PCoIP Standard agent
+
+To deploy a Teradici PCoIP **Standard** agent, on a [Standard_D2_v2](https://docs.microsoft.com/en-us/azure/virtual-machines/windows/sizes-general) <sup>[1]</sup> type virtual machine, click
+    
+<a target="_blank" href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fteradici%2Fpcoip-agent-azure-templates%2Fmaster%2Fazure-deploy-sa-windows2016.json"><img src="http://azuredeploy.net/deploybutton.png"/></a>
+
+#### Teradici PCoIP Graphics agent
+
+To deploy a Teradici PCoIP **Graphics** agent, on a [NV6](https://docs.microsoft.com/en-us/azure/virtual-machines/windows/sizes-gpu) <sup>[1]</sup> type virtual machine, click
+
+<a target="_blank" href="https://portal.azure.com/#create/Microsoft.Template/uri/https%3A%2F%2Fraw.githubusercontent.com%2Fteradici%2Fpcoip-agent-azure-templates%2Fmaster%2Fazure-deploy-ga-windows2016.json"><img src="http://azuredeploy.net/deploybutton.png"/></a>
+
+
+Next follow these steps:
+
+* When prompted by the Azure portal, enter your Azure credentials.
+* When presented with the Azure Custom deployment screen, set these options
+    * *Subscription:* Select your subscription.
+    * *Resource group:* Select *Create new*, then enter a resource group name (for example *teradici_trial*).
+    * *Location:* Select a location. 
+        * Note: Teradici PCoIP Graphics agent deployments require locations that support NV instance types, currently this is limited to the following locations: EAST US, NORTH CENTRAL US, SOUTH CENTRAL US, SOUTHEAST ASIA, or WEST EUROPE.
+    * *Admin User:* Enter a username <sup>[2]</sup> for the virtual machine, this will be used for your PCoIP session connection login.
+    * *Admin Password:* Enter a password <sup>[2]</sup> for the virtual machine.
+    * *Registration Code:* Enter the Registration code you received from Teradici.
+* Review the Microsoft Terms and Conditions. Indicate your agreement by selecting *I agree to the terms and conditions stated above*.
+* Enable *Pin to dashboard* if you wish to monitor the status of your deployment from the Azure dashboard.
+* Click **Purchase**
+
+Deployment will now start and may take up to 15 minutes to complete. You can track the status of the deployment via the Azure Notifications drop down. Regular Azure charges will apply to your Microsoft Azure account for this deployment.
+
+##### Footnotes
+
+ <sup>[1]</sup> see the [Azure Pricing Guide](https://azure.microsoft.com/pricing/details/virtual-machines/windows/) for estimated virtual machine pricing.
+
+ <sup>[2]</sup> see the [FAQs about Windows Virtual Machines](https://docs.microsoft.com/en-us/azure/virtual-machines/windows/faq) for Azure virtual machine username and password requirements.
+
+## Connect to the PCoIP Agent
+
+Once deployment is complete, to connect to your deployed Teradici PCoIP agent find the IP address:
+* Log in to the [Azure Portal](https://portal.azure.com/).
+* Click *Resource groups*, find the resource group you created during deployment, and select it.
+* Click the *Public IP address* resource (named *pcoip-agent-pip*) to obtain the IP address.
+
+From the Teradici PCoIP client, establish a connection to the virtual machine with the Teradici PCoIP agent you deployed on Azure:
+* Launch the Teradici PCoIP client and enter the IP address obtained above in the *Host Address* field, and click **Next**.
+* Enter the *username* and *password* for the virtual machine created above, and click **Login**.
+* You now have access to a PCoIP cloud-delivered desktop.
+
+#### Known Issue(s)
+* Initial, Windowed mode, connections to a newly deployed Teradici PCoIP **Graphics** agent, may result in a blank screen. To clear the blank screen, resize the window or connect in Full Screen mode.
+
+## Delete the PCoIP Agent
+
+When you are finished using the Teradici PCoIP agent (and in order to avoid unwanted charges), use these steps to remove the Teradici PCoIP agent (and all associated resources) from your Microsoft Azure subscription:
+* Log in to the [Azure Portal](https://portal.azure.com/) and click *Resource groups*.
+* Find and right-click the resource group name you created and choose **Delete**.
+* Enter the resource group name when prompted and click **Delete**.
+
+## More Information
+
+* [Teradici Cloud Access Software](http://www.teradici.com/products-and-solutions/pcoip-products/cloud-access-software)
+* [Teradici Cloud Access Software documentation (v2.8)](https://techsupport.teradici.com/ics/support/kbanswer.asp?deptID=15164&task=knowledge&questionID=3090) which includes release notes, quick start guides, etc...
+* [Teradici Technical Support](https://techsupport.teradici.com)
+* [Teradici Community Forum](https://communities.teradici.com/topics/cloud+access+software.html)
+
+## Credits
+
+* Some content in this article is based on the Azure quick start Templates, © Microsoft Azure, licensed under the [MIT license](https://github.com/Azure/azure-quickstart-templates/blob/master/LICENSE).
+
+<p>&nbsp;</p>
+© 2017 Teradici Corporation. All Rights Reserved.
+<p>&nbsp;</p>
---
+++ dsc/Install-PCoIPAgent.ps1
@@ -0,0 +1,246 @@
+# Install-PCoIPAgent.ps1
+Configuration InstallPCoIPAgent
+{
+	param(
+     	[Parameter(Mandatory=$true)]
+     	[String] $pcoipAgentInstallerUrl,
+
+     	[Parameter(Mandatory=$false)]
+     	[String] $videoDriverUrl,
+    
+     	[Parameter(Mandatory=$true)]
+     	[PSCredential] $registrationCodeCredential
+	)
+    
+    $isSA = [string]::IsNullOrWhiteSpace($videoDriverUrl)
+
+    $regPath = If ($isSA) {
+				    "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\PCoIP Standard Agent"
+			   }
+			   Else {
+					"HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\PCoIP Graphics Agent"
+			   }
+	
+    Node "localhost"
+    {
+        VmUsability TheVmUsability
+
+        LocalConfigurationManager
+        {
+            RebootNodeIfNeeded = $true
+        }
+
+        File Agent_Download_Directory 
+        {
+            Ensure          = "Present"
+            Type            = "Directory"
+            DestinationPath = "C:\WindowsAzure\PCoIPAgentInstaller"
+        }
+
+        File Nvidia_Download_Directory 
+        {
+            Ensure          = "Present"
+            Type            = "Directory"
+            DestinationPath = "C:\WindowsAzure\NvidiaInstaller"
+        }
+
+        Script InstallVideoDriver
+        {
+            DependsOn  = "[File]Nvidia_Download_Directory"
+
+            GetScript  = { @{ Result = "Install_Video_Driver" } }
+
+            TestScript = {
+                $isSA = $using:isSA
+
+                if ($isSA -or (Test-Path -path "HKLM:\SOFTWARE\NVIDIA Corporation\Installer2\Drivers")) {
+					return $true
+				}else {
+					return $false
+				} 
+			}
+
+            SetScript  = {
+                Write-Verbose "Downloading Nvidia driver"
+                $videoDriverUrl = $using:videoDriverUrl
+                $installerFileName = [System.IO.Path]::GetFileName($videoDriverUrl)
+                $destFile = "c:\WindowsAzure\NvidiaInstaller\" + $installerFileName
+                Invoke-WebRequest $videoDriverUrl -OutFile $destFile
+
+                Write-Verbose "Installing Nvidia driver"
+                $ret = Start-Process -FilePath $destFile -ArgumentList "/s /noeula /noreboot" -PassThru -Wait
+                Write-Verbose ("Nvidia driver exit code: "  + $ret.ExitCode)
+
+                # treat returned code 0 and 1 as success
+				if (($ret.ExitCode -ne 0) -and ($ret.ExitCode -ne 1)) {
+                    $stdout = $ret.StandardOutput.ReadToEnd();
+                    $stderr = $ret.StandardError.ReadToEnd();
+					$errMsg = "Failed to install nvidia driver. standard output: " + $stdout + "; standard error: " + $stderr
+					Write-Verbose $errMsg
+					throw $errMsg
+				} else {
+					Write-Verbose "Request reboot machine after Installing Video Driver."
+					# Setting the global:DSCMachineStatus = 1 tells DSC that a reboot is required
+					$global:DSCMachineStatus = 1
+				}
+
+                Write-Verbose "Finished Nvidia driver Installation"
+            }
+        }
+
+        Script Install_PCoIPAgent
+        {
+            DependsOn  = @("[File]Agent_Download_Directory","[Script]InstallVideoDriver")
+            GetScript  = { @{ Result = "Install_PCoIPAgent" } }
+
+            #TODO: Check for other agent types as well?
+            TestScript = {
+                $regPath = $using:regPath
+				if ( Test-Path -path $regPath)  {
+					return $true
+				}else {
+					return $false
+				} 
+			}
+
+            SetScript  = {
+                Write-Verbose "Starting to Install PCoIPAgent"
+
+				#agent installer exit code 1641 require reboot machine
+				Set-Variable EXIT_CODE_REBOOT 1641 -Option Constant
+
+                $pcoipAgentInstallerUrl = $using:pcoipAgentInstallerUrl
+                $installerFileName = [System.IO.Path]::GetFileName($pcoipAgentInstallerUrl)
+                $destFile = "C:\WindowsAzure\PCoIPAgentInstaller\" + $installerFileName
+                
+				Write-Verbose "Downloading PCoIP Agent"
+                Invoke-WebRequest $pcoipAgentInstallerUrl -OutFile $destFile
+
+                #install the agent
+				Write-Verbose "Installing PCoIP Agent"
+                $ret = Start-Process -FilePath $destFile -ArgumentList "/S /nopostreboot" -PassThru -Wait
+
+				# Check installer return code
+				if ($ret.ExitCode -ne 0) {
+					#exit code 1641 means requiring reboot machine after intallation is done, other non zere exit code means installation has some error
+					if ($ret.ExitCode -eq $EXIT_CODE_REBOOT) {
+						Write-Verbose "Request reboot machine after Installing pcoip agent."
+						# Setting the global:DSCMachineStatus = 1 tells DSC that a reboot is required
+						$global:DSCMachineStatus = 1
+					} else {
+						$errMsg = "Failed to install PCoIP Agent. Exit Code: " + $ret.ExitCode
+						Write-Verbose $errMsg
+						throw $errMsg
+					}
+				}
+				
+	            Write-Verbose "Finished PCoIP Agent Installation"
+            }
+        }
+
+        Script Register
+        {
+            DependsOn  = @("[Script]Install_PCoIPAgent")
+
+            GetScript  = { return 'registration'}
+            
+            TestScript = { 
+                cd "C:\Program Files (x86)\Teradici\PCoIP Agent"
+ 	            $ret = & .\pcoip-validate-license.ps1
+
+				# the powershell variable $? to indicate the last executing command status
+				return $?
+            }
+
+            SetScript  = {
+                #register code is stored at the password property of PSCredential object
+                $registrationCode = ($using:registrationCodeCredential).GetNetworkCredential().password
+                if ($registrationCode) {
+					# Insert a delay before activating license
+	                cd "C:\Program Files (x86)\Teradici\PCoIP Agent"
+
+					Write-Verbose "Activating License Code"               
+ 	                $ret = & .\pcoip-register-host.ps1 -RegistrationCode $registrationCode
+					$isExeSucc = $?
+
+					if ($isExeSucc) {
+						Write-Verbose "succeeded to activate License Code." 
+					} else {
+						$retMsg = $ret | Out-String
+						$errMsg = "Failed to activate License Code because " + $retMsg
+						Write-Verbose  $errMsg              
+						throw $errMsg
+					}
+
+					Write-Verbose "Validating License"               
+ 	                $ret = & .\pcoip-validate-license.ps1
+					$isExeSucc = $?
+
+					if ($isExeSucc) {
+						Write-Verbose "succeeded to validate License."
+					} else {
+						$retMsg = $ret | Out-String
+						$errMsg = "Failed to validate license because " + $retMsg
+						Write-Verbose  $errMsg              
+						throw $errMsg
+					}
+                }
+               
+				#start service if it is not started
+				$serviceName = "PCoIPAgent"
+				$svc = Get-Service -Name $serviceName   
+
+				if ($svc.StartType -eq "Disabled") {
+					Set-Service -name  $serviceName -StartupType Automatic
+				}
+					
+				if ($svc.status -eq "Paused") {
+					$svc.Continue()
+				}
+
+				if ( $svc.status -eq "Stopped" )	{
+					Write-Verbose "Starting PCoIP Agent Service because it is at stopped status."
+					$svc.Start()
+					$svc.WaitForStatus("Running", 120)
+				}
+            }
+        }
+    }
+}
+
+Configuration VmUsability
+{
+    Node "localhost"
+    {
+        DisableServerManager TheDisableServerManager
+        InstallFirefox TheInstallFirefox
+    }
+}
+
+Configuration DisableServerManager
+{
+    Node "localhost"
+    {
+        Registry DisableServerManager
+        {
+            Ensure = "Present"
+            Key = "HKLM:\Software\Microsoft\ServerManager"
+            ValueName = "DoNotOpenServerManagerAtLogon"
+            ValueData = "1"
+            ValueType = "Dword"
+        }
+    }
+}
+
+Configuration InstallFirefox
+{
+    Import-DscResource -module xFirefox
+
+    Node "localhost"
+    {
+        MSFT_xFirefox InstallFirefox
+        {
+            #install the latest firefox browser
+        }
+    }
+}
